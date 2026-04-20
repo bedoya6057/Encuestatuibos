537a53680c4dd55edbba3be708d39679d305b691
Sodexo User <sodexo@example.com>
2026-04-20 10:55:19 -0500

    Update_Title

--- git_push.bat
+++
@@ -1,6 +0,0 @@
-git init
-git add .
-git commit -m "Initial commit - migrated to Supabase"
-git branch -M main
-git remote add origin https://github.com/bedoya6057/Encuestatuibos.git
-git push -u origin main --force
